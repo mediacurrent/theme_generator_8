09e9f640d22efe1e7aa706163cd6ee12bcb4d22c
Tregonia <tim.dickens@mediacurrent.com>
2020-01-13 17:53:26 -0500

    Integrating Stylelint as a replacement for Sass-lint

--- generators/app/index.js
+++ generators/app/index.js
@@ -180,8 +180,8 @@ module.exports = class extends Generator {
       this.destinationPath('.eslintrc.yml')
     );
     this.fs.copy(
-      this.templatePath('sass-lint.yml'),
-      this.destinationPath('.sass-lint.yml')
+      this.templatePath('stylelintrc.yml'),
+      this.destinationPath('.stylelintrc.yml')
     );
     // We need the theme machine name so we can set
     // correct namespaces.
--- generators/app/templates/_gulp-tasks/lint.js
+++ generators/app/templates/_gulp-tasks/lint.js
@@ -5,19 +5,27 @@
 const { src } = require('gulp');
 
 // Include Our Plugins
-const sassLint = require('gulp-sass-lint');
+const gulpStylelint = require('gulp-stylelint');
 const eslint = require('gulp-eslint');
 
 // Export our tasks.
 module.exports = {
-  // Lint Sass based on .sass-lint.yml config.
-  lintSass: function() {
+  // Lint Sass based on .stylelintrc.yml config.
+  lintSass: function () {
     return src([
       './src/patterns/{global,layout,components}/**/*.scss',
       '!./src/patterns/global/utils/*'
     ])
-      .pipe(sassLint())
-      .pipe(sassLint.format());
+      .pipe(
+        gulpStylelint({
+          reporters: [
+            {
+              formatter: 'string',
+              console: true
+            }
+          ]
+        })
+      );
   },
 
   // Lint JavaScript based on .eslintrc config.
