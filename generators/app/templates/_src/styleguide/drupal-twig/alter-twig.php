ef966782d438262bc6a71f6187f769022f6728a2
ZackHawkinsMC <zack.hawkins@mediacurrent.com>
2020-04-08 12:29:38 -0400

    Spring Updates
    - Fixes ESlint max-len so it doesn't flag urls, strings, regex and
      template literals.
    - Fixes code sniffer violations for PHP files used by Pattern Lab node.
    - Backs out composer automated install for SlickJS and suggests next
      steps instead.
    - Updates heading component to be more flexible by accepting any element
      not just headings.
    - Adds Drupal integration for card and card-list components.

--- generators/app/templates/_src/styleguide/drupal-twig/alter-twig.php
+++ generators/app/templates/_src/styleguide/drupal-twig/alter-twig.php
@@ -1,16 +1,30 @@
 <?php
 
 /**
- * @param Twig_Environment $env - The Twig Environment - https://twig.symfony.com/api/1.x/Twig_Environment.html
- * @param $config - Config of `@basalt/twig-renderer`
+ * @file
+ * Loads BasicTwigExtensions for Pattern Lab Node.
  */
-function twigExtensions(\Twig_Environment &$env, $config) {
 
-  // Load the \BasicTwigExtensions class so the extension can be added correctly.
+use PatternLabTwigExtensions\BasicTwigExtensions;
+
+/**
+ * Enable Drupal Twig extensions for Pattern Lab.
+ *
+ * @param Twig_Environment $env
+ *   - The Twig Environment
+ *   - https://twig.symfony.com/api/1.x/Twig_Environment.html.
+ * @param object $config
+ *   - Config of `@basalt/twig-renderer`.
+ */
+function twig_extensions(Twig_Environment &$env, $config) {
+
+  // Load the \BasicTwigExtensions class so the extension
+  // can be added correctly.
   spl_autoload_register(function ($class_name) {
-    include __DIR__ . '/' . $class_name . '.php';
+    $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
+    include_once __DIR__ . '/' . $class_name . '.php';
   });
 
   $env->addExtension(new \Twig_Extension_Debug());
-  $env->addExtension(new \BasicTwigExtensions());
+  $env->addExtension(new BasicTwigExtensions());
 }
